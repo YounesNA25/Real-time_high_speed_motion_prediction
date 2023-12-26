d3f73cf9f0539da12f471a717871ef56d8b82aa9
MassylHAMDI <116282135+MassylHAMDI@users.noreply.github.com>
2023-12-26 22:55:00 +0100

    Update test.py

--- test.py
+++ test.py
@@ -48,14 +48,67 @@ def compute_local_flow(events, neighborhood_size=5):
 
     return local_flows
 
-# Dummy functions for plane fitting and getting neighborhood - To be replaced with actual implementations
-def plane_fitting(x, y, t, size):
-    # Implement the plane fitting algorithm or use a library
-    return [0.1, 0.1, 0.1]  # example coefficients a, b, c
+import numpy as np
 
-def get_neighborhood(x, y, t, size):
-    # Implement a way to get neighborhood events
-    return [(x, y, t)]  # dummy return
+def plane_fitting(x: np.ndarray, y: np.ndarray, ts: np.ndarray):
+    """
+    Performs a least squares plane fit to the given points.
+    
+    @param x: np.ndarray of x coordinates.
+    @param y: np.ndarray of y coordinates.
+    @param ts: np.ndarray of time stamps or third dimension coordinates.
+    
+    @return: (a, b, c) coefficients of the fitted plane of the form z = ax + by + c.
+             Returns None if fitting is not possible.
+    """
+    # Construct the design matrix with x, y, and a column of ones for the intercept term.
+    A = np.c_[x, y, np.ones(x.shape[0])]
+    
+    # Use least squares to solve for the plane coefficients.
+    # np.linalg.lstsq returns several values; we only want the coefficients here.
+    coefficients, _, _, _ = np.linalg.lstsq(A, ts, rcond=None)
+    
+    # coefficients are [a, b, c] of the plane equation.
+    return coefficients if coefficients.size else None
+
+# Example usage
+# Assuming x, y, ts are defined as arrays of your data points
+# plane = plane_fitting(x, y, ts)
+# print(plane)  # Outputs: array([a, b, c])
+
+
+def get_neighborhood(x: np.ndarray, y: np.ndarray, ts: np.ndarray, event: int, N: int = 3, dt: int = 1000):
+    """
+    Determines the neighborhood of a given event within spatial and temporal constraints.
+    
+    @param x: np.ndarray of x coordinates.
+    @param y: np.ndarray of y coordinates.
+    @param ts: np.ndarray of timestamps.
+    @param event: The index of the event around which to find the neighborhood.
+    @param N: Spatial neighborhood size (distance in both x and y).
+    @param dt: Temporal window size (time before and after the event timestamp).
+    
+    @return: Indices of the events in the neighborhood.
+    """
+    # Convert ts to microseconds for comparison and define time window
+    ts_micro = ts * 10**6  # Assuming ts is in seconds and needs conversion to microseconds
+    event_time = ts_micro[event]
+    
+    # Define temporal window around the event
+    index_right = np.searchsorted(ts_micro, event_time + dt, side='right')
+    index_left = np.searchsorted(ts_micro, event_time - dt, side='left')
+    time_window = np.arange(index_left, index_right, 1)
+
+    # Define spatial window around the event
+    spatial_window = np.where(
+        (x[event] - N <= x[time_window]) & (x[time_window] <= x[event] + N) &
+        (y[event] - N <= y[time_window]) & (y[time_window] <= y[event] + N)
+    )[0]
+
+    # The neighborhood is the intersection of the spatial and temporal window
+    neighborhood = time_window[spatial_window]
+
+    return neighborhood
 
 # Example usage
 events = [(1, 2, 0.1), (2, 3, 0.2)]  # Define your events list
